2f2ea220d66beb6534a7fb9d1a14ff4aaab0211e
Oleksandr-Kalinin <work.kalinin.o.v@gmail.com>
2024-12-17 16:31:12 +0200

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout.jsx";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={"<HomePage />"} />
+        <Route path="/catalog" element={"<TrucksPage />"} />
+        <Route path="/catalog/:id" element={"<TruckDetailsPage />"}>
+          <Route path="features" element={"<TruckFeatures />"} />
+          <Route path="reviews" element={"<TruckReviews />"} />
+        </Route>
+        <Route path="*" element={<Navigate to={"/"} replace />} />
+      </Routes>
+    </Layout>
+  );
 };
 
 export default App;
