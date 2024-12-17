5117c9bcb315c75383a2804ff58b2775b0dd0d61
Oleksandr-Kalinin <work.kalinin.o.v@gmail.com>
2024-12-17 17:04:55 +0200

    add component Section

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,8 @@
+import clsx from "clsx";
+import css from "./Section.module.css";
+
+const Section = ({ children, className }) => {
+  return <section className={clsx(css.section, className)}>{children}</section>;
+};
+
+export default Section;
